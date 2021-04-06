f636a3e5407b74791bfb5407ef8e798f6cd5fdda
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-06 19:57:33 +0900

    :tada: (shared) Initial commit

---
+++ packages/shared/src/components/loading/constants.ts
@@ -0,0 +1,8 @@
+export const DEFAULT_STROKE = 6
+export const DEFAULT_RADIUS = 32
+export const DEFAULT_COLOR = '#3051ff'
+export const PATH_LENGTH = 100
+export const classNames = {
+  loading: 'loading',
+  loadingCircle: 'loading-circle'
+}
