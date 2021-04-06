33466f4801434fc8d70d91ddd89d53110f358b4e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-06 20:35:27 +0900

    :tada: (react) Initial commit

---
+++ packages/react/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  },
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src', 'index.ts'),
+      name: 'Xross'
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        exports: 'named',
+        sourcemap: true,
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
+})
