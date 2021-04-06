d57d1710f01aca58ce68b0d0838f9e25b1eb1358
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-06 20:23:43 +0900

    :tada: (docs) Initial commit

---
+++ docs/vue/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <Loading />
+</template>
+
+<script setup lang="ts">
+import { Loading } from 'xross-vue'
+</script>
---
+++ docs/vue/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '../../../packages/vue/dist/style.css'
+
+createApp(App).mount('#app')
