b8b2bcf42add59bd6e03037749bf5accc3c108c6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-07 10:20:28 +0900

    :sparkles: (react) Add loading component

--- packages/react/src/components/loading/Loading.tsx
+++ packages/react/src/components/loading/Loading.tsx
@@ -1,3 +1,53 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React, { FC, useMemo } from 'react'
+import '@share/components/loading/index.less'
+import {
+  DEFAULT_RADIUS,
+  DEFAULT_STROKE,
+  DEFAULT_COLOR,
+  PATH_LENGTH,
+  classNames
+} from '@share/components/loading/constants'
 
-export default () => <div>hello</div>
+const unit = (unit: string, val: string): string => `${val}${unit}`
+
+type Props = Partial<{
+  stroke: number
+  radius: number
+  color: string
+}>
+
+const Loading: FC<Props> = ({
+  stroke = DEFAULT_STROKE,
+  radius = DEFAULT_RADIUS,
+  color = DEFAULT_COLOR
+}) => {
+  const normalizedRadius = useMemo(() => radius - stroke / 2, [radius, stroke])
+  const { loading, loadingCircle } = classNames
+  const diameter = useMemo(() => radius * 2, [radius])
+  const diameterpx = useMemo(() => unit('px', diameter.toString()), [diameter])
+
+  return (
+    <svg
+      className={loading}
+      style={{
+        maxWidth: diameterpx,
+        maxHeight: diameterpx
+      }}
+    >
+      <circle
+        className={loadingCircle}
+        stroke={color}
+        strokeWidth={stroke}
+        strokeLinecap="round"
+        fill="none"
+        r={normalizedRadius}
+        cx={radius}
+        cy={radius}
+        pathLength={PATH_LENGTH}
+      />
+    </svg>
+  )
+}
+
+export default Loading
