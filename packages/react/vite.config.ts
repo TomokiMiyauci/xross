2b439c4f7754e9aa7e8e17f40dca3cd39d1150cd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-07 10:20:01 +0900

    :wrench: (react) Add using shared project setting

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -9,6 +9,10 @@ export default defineConfig({
       {
         find: '@',
         replacement: resolve(__dirname, 'src')
+      },
+      {
+        find: '@share',
+        replacement: resolve(__dirname, '..', 'shared', 'src')
       }
     ]
   },
